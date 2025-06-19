846f3c0327ab7827924aeea525932a3cb1011cfa
sophie_kao <sophie_kao@sonix.com.tw>
2025-06-19 13:52:21 +0800

    Base on Burst mode, add NTC error function
    (cherry picked from commit be1b73240e720943d55e45cc9221cf78f027cea3)
    
    # Conflicts:
    #	PWM.c

--- temperature.h
+++ temperature.h
@@ -9,12 +9,17 @@
 /*_____ D E C L A R A T I O N S ____________________________________________*/
 extern int idata IGBT_TEMP_C;      // 目前IGBT溫度
 extern int idata TOP_TEMP_C;       // 目前表面溫度
+extern bit    f_ntc_monitoring;
+extern uint8_t ntc_change_count;
+extern uint16_t ntc_monitor_start_time_s;
+extern int    last_IGBT_temp;
 
 /*_____ M A C R O S ________________________________________________________*/
 
 /*_____ F U N C T I O N S __________________________________________________*/
 void Temp_Measure(void );
 void Temp_Process(void);
+void Print_All_IGBT_Temps(void);
 
 
 #endif  // __TEMP_H__
